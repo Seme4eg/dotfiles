c90808a3e964ed620711a9288df2ca60d69c9583
sad <d3v1ant@mail.ru>
2022-08-14 17:24:36 +0300

    fix timeshift error, now i have backup \o/

--- .config/hypr/autorun.sh
+++ .config/hypr/autorun.sh
@@ -11,6 +11,7 @@ nm-applet --indicator &
 pkexec /usr/bin/brillo -c 2
 pkexec /usr/bin/brillo -I
 wlsunset -l 55.7 -L 37.6 -t 3000 &
+/usr/lib/polkit-kde-authentication-agent-1 &
 
 # -i <img> -s fill
 swayidle -w \
