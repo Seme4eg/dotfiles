79a2068da2d1a393d1b8558d5ca16f575378653c
sad <d3v1ant@mail.ru>
2023-01-16 02:06:59 +0300

    hyprpaper + setwp script fixed

--- .config/hypr/setwp.sh
+++ .config/hypr/setwp.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
 wp=$(find ~/Pictures/atmosphere/Wadim\ Kashin/ -type f | shuf -n 1)
-configFile=~/.config/hypr/hyprpaper.conf
 
-pkill -9 hyprpaper
+[[ $prev_wallpaper ]] && hyprctl hyprpaper unload "$prev_wallpaper"
+[[ -z $(pgrep hyprpaper) ]] && hyprpaper &
 
-> $configFile
-echo "preload = $wp" 1>> $configFile
-echo "wallpaper = eDP-1,$wp" 1>> $configFile
+hyprctl hyprpaper preload "$wp"
+hyprctl hyprpaper wallpaper "eDP-1,$wp"
 
-hyprpaper &
+prev_wallpaper=$wp
+
+# ... if the above won't work - fix your system
+# as the shitty workaround:
+
+# wp=$(find ~/Pictures/atmosphere/Wadim\ Kashin/ -type f | shuf -n 1)
+# configFile=~/.config/hypr/hyprpaper.conf
+
+# pkill -9 hyprpaper
+
+# > $configFile
+# echo "preload = $wp" 1>> $configFile
+# echo "wallpaper = eDP-1,$wp" 1>> $configFile
+
+# hyprpaper &
