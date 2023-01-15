ec8f77aa1cff2818b09cff1a32500a0385a14eaf
sad <d3v1ant@mail.ru>
2023-01-15 03:38:23 +0300

    massive hyprland upgrade

--- .config/hypr/autorun.sh
+++ .config/hypr/autorun.sh
@@ -1,11 +1,13 @@
-qutebrowser &
+firefox &
 webcord --enable-features=UseOzonePlatform --ozone-platform=wayland &
 emacs &
-syncthing &
-# for mako
+syncthing & # XXX setup syncthing tray & syncthing service on startup maybe?
+
+# for mako notifications
 dbus-daemon --session --address=unix:path=$XDG_RUNTIME_DIR/bus &
-dbus-launch # XXX
+#dbus-launch # XXX remove when telega is ok
 mako &
+
 hyprpaper &
 waybar &
 nm-applet --indicator &
@@ -13,10 +15,12 @@ brillo -c 2
 brillo -I
 wlsunset -l 55.7 -L 37.6 -t 3000 &
 /usr/lib/polkit-kde-authentication-agent-1 &
+/usr/libexec/xdg-desktop-portal-hyprland &
+# sleep 2
+# /usr/lib/xdg-desktop-portal &
 
-# -i <img> -s fill
+# TODO: doesn't seem to work
+# i <img> -s fill
 swayidle -w \
-	timeout 300 'swaylock -f -e -k -l -c 000000' \
 	timeout 600 'swaymsg "output * dpms off"' \
-		resume 'swaymsg "output * dpms on"' \
-	before-sleep 'swaylock -f -e -k -l -c 000000' &
+		resume 'swaymsg "output * dpms on"' &
