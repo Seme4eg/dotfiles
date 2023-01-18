260a0ac3c9c7a35c2998ee4d969a9c50e31dfef8
sad <d3v1ant@mail.ru>
2023-01-19 00:50:06 +0300

    adopt autorun script for soystemd

--- .config/hypr/autorun.sh
+++ .config/hypr/autorun.sh
@@ -1,19 +1,27 @@
-# for mako notifications
-dbus-daemon --session --address=unix:path=$XDG_RUNTIME_DIR/bus &
-mako &
+# for mako notifications if not on systemd
+# dbus-daemon --session --address=unix:path=$XDG_RUNTIME_DIR/bus &
+# mako &
+
+# FIXME: thing below fails, try to run it in terminal
+# dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY HYPRLAND_INSTANCE_SIGNATURE XDG_CURRENT_DESKTOP
+systemctl --user start hyprland-session.target
+
 /usr/lib/polkit-kde-authentication-agent-1 &
-/usr/libexec/xdg-desktop-portal-hyprland &
-# sleep 2
-# /usr/lib/xdg-desktop-portal &
+# /usr/libexec/xdg-desktop-portal-hyprland & # ain't needed with soystemd
 
 firefox &
 webcord --enable-features=UseOzonePlatform --ozone-platform=wayland &
 emacs &
-syncthing & # XXX setup syncthing tray & syncthing service on startup maybe?
 
-hyprpaper &
 waybar &
-nm-applet --indicator &
-brillo -c 2
-brillo -I
+nm-applet --indicator & # TODO: remove it when ya'll setup own wireless widget
+
+pkexec /usr/bin/brillo -c 2
+pkexec /usr/bin/brillo -I
+
 wlsunset -l 55.7 -L 37.6 -t 3000 &
+
+# TODO: setup waybars idle inhibitor or a shortcut to on/off it so the screen
+# doesn't turn off when im watching something
+# i <img> -s fill
+# swayidle timeout 300 'swaylock -f -e -k -l -c 000000' timeout 600 'swaymsg "/usr/bin/hyprctl dispatch dpms off"' resume 'swaymsg "/usr/bin/hyprctl dispatch dpms on"' before-sleep '/usr/bin/swaylock -f -e -k -l -c 000000' &
