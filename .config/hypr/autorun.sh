0c3f98797e9993060108d07ebf002a0746d2ea27
sad <d3v1ant@mail.ru>
2023-01-19 03:49:53 +0300

    keep on cleaning autorun

--- .config/hypr/autorun.sh
+++ .config/hypr/autorun.sh
@@ -2,8 +2,7 @@
 # dbus-daemon --session --address=unix:path=$XDG_RUNTIME_DIR/bus &
 # mako &
 
-# FIXME: thing below fails, try to run it in terminal
-# dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY HYPRLAND_INSTANCE_SIGNATURE XDG_CURRENT_DESKTOP
+dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY HYPRLAND_INSTANCE_SIGNATURE XDG_CURRENT_DESKTOP
 systemctl --user start hyprland-session.target
 
 /usr/lib/polkit-kde-authentication-agent-1 &
@@ -13,15 +12,7 @@ firefox &
 webcord --enable-features=UseOzonePlatform --ozone-platform=wayland &
 emacs &
 
-waybar &
-nm-applet --indicator & # TODO: remove it when ya'll setup own wireless widget
+# nm-applet --indicator & # TODO: remove it when ya'll setup own wireless widget
 
 pkexec /usr/bin/brillo -c 2
 pkexec /usr/bin/brillo -I
-
-wlsunset -l 55.7 -L 37.6 -t 3000 &
-
-# TODO: setup waybars idle inhibitor or a shortcut to on/off it so the screen
-# doesn't turn off when im watching something
-# i <img> -s fill
-# swayidle timeout 300 'swaylock -f -e -k -l -c 000000' timeout 600 'swaymsg "/usr/bin/hyprctl dispatch dpms off"' resume 'swaymsg "/usr/bin/hyprctl dispatch dpms on"' before-sleep '/usr/bin/swaylock -f -e -k -l -c 000000' &
