99a47eb5b90a67918adbf9299024c86ada540364
earthian <d3v1ant@mail.ru>
2023-01-27 22:55:22 +0300

    move hyprpaper back from soystemd service

--- .config/hypr/setwp.sh
+++ .config/hypr/setwp.sh
@@ -3,22 +3,10 @@
 wp=$(find ~/Pictures/atmosphere/Wadim\ Kashin/ -type f | shuf -n 1)
 
 [[ $prev_wallpaper ]] && hyprctl hyprpaper unload "$prev_wallpaper"
+# remove this line if you deside to move hyprpaper to soystemd service again
+[[ -z $(pgrep hyprpaper) ]] && hyprpaper &
 
 hyprctl hyprpaper preload "$wp"
 hyprctl hyprpaper wallpaper "eDP-1,$wp"
 
 prev_wallpaper=$wp
-
-# ... if the above won't work - fix your system
-# as the shitty workaround:
-
-# wp=$(find ~/Pictures/atmosphere/Wadim\ Kashin/ -type f | shuf -n 1)
-# configFile=~/.config/hypr/hyprpaper.conf
-
-# pkill -9 hyprpaper
-
-# > $configFile
-# echo "preload = $wp" 1>> $configFile
-# echo "wallpaper = eDP-1,$wp" 1>> $configFile
-
-# hyprpaper &
