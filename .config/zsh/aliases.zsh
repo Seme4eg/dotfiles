9e624d0ec8531c1f70af04c2066061618c623c7b
earthian <d3v1ant@mail.ru>
2023-01-30 20:15:47 +0300

    fix yay package completions

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -31,6 +31,9 @@ alias yU="yay -Syyu"
 alias yu="yay -Syu"
 # -Scc is more agressive approach, read bout it before using in case u decide to
 alias ycc="yay -Sc" # Clear Cache
+# force a refresh of the package cache used for completions if it broke after
+# above command
+alias yfc="yay -Pcc" # yay fix cache
 
 # --- Utilities ---
 alias cat="bat"
