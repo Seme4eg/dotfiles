2b96dad650a439c94874464bf967f355cd429acd
earthian <d3v1ant@mail.ru>
2023-01-31 21:27:15 +0300

    clean autorun.sh

--- .config/hypr/autorun.sh
+++ .config/hypr/autorun.sh
@@ -1,8 +1,10 @@
-dbus-update-activation-environment --systemd HOME DISPLAY WAYLAND_DISPLAY HYPRLAND_INSTANCE_SIGNATURE XDG_CURRENT_DESKTOP
+dbus-update-activation-environment --systemd \
+  HOME DISPLAY WAYLAND_DISPLAY HYPRLAND_INSTANCE_SIGNATURE XDG_CURRENT_DESKTOP
+
 systemctl --user start hyprland-session.target
 
 /usr/lib/polkit-kde-authentication-agent-1 &
-/usr/libexec/xdg-desktop-portal-hyprland & # ain't needed with soystemd
+# /usr/libexec/xdg-desktop-portal-hyprland & # ain't needed with soystemd
 
 hyprctl setcursor Layan-cursors 26
 
