87e19226f657a7e3fa97de02f4d970b76049ef9d
earthian <d3v1ant@mail.ru>
2023-02-12 01:31:09 +0300

    fix error in autorun sh

--- .config/hypr/autorun.sh
+++ .config/hypr/autorun.sh
@@ -8,7 +8,7 @@ systemctl --user start hyprland-session.target
 
 hyprctl setcursor Future-dark-cursors 26
 
-wl-paste --watch cliphist store
+wl-paste --watch cliphist store &
 
 firefox &
 webcord --enable-features=UseOzonePlatform --ozone-platform=wayland &
