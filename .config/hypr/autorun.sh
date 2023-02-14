a3893e82dcd02a3fbd1821b5d2f5a58a9cf96dc4
earthian <d3v1ant@mail.ru>
2023-02-14 19:49:26 +0300

    cursor not applying fixed?

--- .config/hypr/autorun.sh
+++ .config/hypr/autorun.sh
@@ -1,11 +1,12 @@
+/usr/lib/polkit-kde-authentication-agent-1 &
 dbus-update-activation-environment --systemd \
   HOME DISPLAY WAYLAND_DISPLAY HYPRLAND_INSTANCE_SIGNATURE XDG_CURRENT_DESKTOP
 
 systemctl --user start hyprland-session.target
 
-/usr/lib/polkit-kde-authentication-agent-1 &
 # /usr/libexec/xdg-desktop-portal-hyprland & # ain't needed with soystemd
 
+gsettings set org.gnome.desktop.interface cursor-theme Future-dark-cursors
 hyprctl setcursor Future-dark-cursors 26
 
 wl-paste --watch cliphist store &
