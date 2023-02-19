10d3684338837aa355f6b51ebfc2fd8bdb2ca68f
earthian <d3v1ant@mail.ru>
2023-02-19 18:50:42 +0300

    support for multifolder wallpapers

--- .config/rofi/scripts/rofi-choose-wp.sh
+++ .config/rofi/scripts/rofi-choose-wp.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-wp_dir="$HOME/Pictures/wps/pc/wadim_kashin"
+wp_dir="$HOME/Pictures/wps/pc"
 
 list_wps() {
-  find "$wp_dir" -maxdepth 1 -type f -printf "%f\n" |
+  find "$wp_dir" -type f -printf "%P\n" |
     while read wp; do
       echo -en "$wp\x00icon\x1f$wp_dir/$wp\n";
     done
