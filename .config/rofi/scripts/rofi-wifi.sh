b28501d8e20358a241c78cccacec1a701b6e3071
earthian <d3v1ant@mail.ru>
2023-02-19 13:47:23 +0300

    finally done with wifi script tho its shitty

--- .config/rofi/scripts/rofi-wifi.sh
+++ .config/rofi/scripts/rofi-wifi.sh
@@ -1,101 +1,74 @@
 #!/usr/bin/env bash
-# https://github.com/zbaylin/rofi-wifi-menu
 
-# Starts a scan of available broadcasting SSIDs
-# nmcli dev wifi rescan
+# notify-send -a $(whoami) "Getting list of available Wi-Fi networks..."
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# some maybe useful fields
+# ACTIVE
+FIELDS=IN-USE,SSID,SECURITY,FREQ,RATE,SIGNAL,BARS
 
-FIELDS=SSID,SECURITY
-POSITION=0
-YOFF=0
-XOFF=0
-FONT="DejaVu Sans Mono 8"
+# awk NF to filter empty lines from output
+wifi_list=$(nmcli --fields "$FIELDS" device wifi list | sed '/--/d' | awk NF)
+# get ssids list, -t flag returns list without 1st 'header' line
+wifi_ssids=$(nmcli -t --fields SSID device wifi list | awk NF)
 
-if [ -r "$DIR/config" ]; then
-	source "$DIR/config"
-elif [ -r "$HOME/.config/rofi/wifi" ]; then
-	source "$HOME/.config/rofi/wifi"
-else
-	echo "WARNING: config file not found! Using default values."
-fi
+# Getting ssid of the chosen row by mapping list or rows and list of ssids
+# to each other. Reason for this function is that i didn't know find another way
+# to parse row to get it's ssid. Because of possible spaces in ssids.
+get_ssid() {
+  # start with 0 index cuz we are also iterating over 'headers' line first
+  index=0
 
-LIST=$(nmcli --fields "$FIELDS" device wifi list | sed '/^--/d')
-# For some reason rofi always approximates character width 2 short... hmmm
-RWIDTH=$(($(echo "$LIST" | head -n 1 | awk '{print length($0); }')+2))
-# Dynamically change the height of the rofi menu
-LINENUM=$(echo "$LIST" | wc -l)
-# Gives a list of known connections so we can parse it later
-KNOWNCON=$(nmcli connection show)
+  echo "$wifi_list" |
+    while read row; do
+      # get 'index'th line from the list of ssids
+      ssid=$(echo "$wifi_ssids" | awk NR==$index)
+
+      # remove all spaces from both strings, maybe not best way to do it idk
+      if [ ${row// /} = ${1// /} ]; then
+        echo "$ssid"
+        break
+      fi
+
+      (( index++ ))
+    done
+}
+
+disable_row="睊  Disable Wi-Fi"
+enable_row="睊  Enable Wi-Fi"
 # Really janky way of telling if there is currently a connection
-CONSTATE=$(nmcli -fields WIFI g)
+[[ $(nmcli -fields WIFI g) =~ "enabled" ]] &&
+  TOGGLE="$disable_row" || TOGGLE="$enable_row"
 
-CURRSSID=$(LANGUAGE=C nmcli -t -f active,ssid dev wifi | awk -F: '$1 ~ /^yes/ {print $2}')
+# force monospace font to not get those fields messy
+chosen_row=$(echo -e "$TOGGLE\n$wifi_list" |
+                uniq -u |
+                rofi -dmenu -selected-row 2 \
+                  -theme-str '#entry { placeholder: "Wi-Fi SSID:"; }' \
+                  -theme-str '* { font: "Source Code Pro 13"; }')
+chosen_ssid=$(get_ssid "$chosen_row")
 
-if [[ ! -z $CURRSSID ]]; then
-	HIGHLINE=$(echo  "$(echo "$LIST" | awk -F "[  ]{2,}" '{print $1}' | grep -Fxn -m 1 "$CURRSSID" | awk -F ":" '{print $1}') + 1" | bc )
-fi
+notify_success() {
+  grep "successfully" &&
+    notify-send -a $(whoami) "Connection Established" \
+      "You are now connected to the Wi-Fi network \"$chosen_ssid\"."
+}
 
-# HOPEFULLY you won't need this as often as I do
-# If there are more than 8 SSIDs, the menu will still only have 8 lines
-if [ "$LINENUM" -gt 8 ] && [[ "$CONSTATE" =~ "enabled" ]]; then
-	LINENUM=8
-elif [[ "$CONSTATE" =~ "disabled" ]]; then
-	LINENUM=1
-fi
-
-
-if [[ "$CONSTATE" =~ "enabled" ]]; then
-	TOGGLE="toggle off"
-elif [[ "$CONSTATE" =~ "disabled" ]]; then
-	TOGGLE="toggle on"
-fi
-
-
-
-CHENTRY=$(echo -e "$TOGGLE\nmanual\n$LIST" | uniq -u | rofi -dmenu -p "Wi-Fi SSID: " -lines "$LINENUM" -a "$HIGHLINE" -location "$POSITION" -yoffset "$YOFF" -xoffset "$XOFF" -font "$FONT" -width -"$RWIDTH")
-#echo "$CHENTRY"
-CHSSID=$(echo "$CHENTRY" | sed  's/\s\{2,\}/\|/g' | awk -F "|" '{print $1}')
-#echo "$CHSSID"
-
-# If the user inputs "manual" as their SSID in the start window, it will bring them to this screen
-if [ "$CHENTRY" = "manual" ] ; then
-	# Manual entry of the SSID and password (if appplicable)
-	MSSID=$(echo "enter the SSID of the network (SSID,password)" | rofi -dmenu -p "Manual Entry: " -font "$FONT" -lines 1)
-	# Separating the password from the entered string
-	MPASS=$(echo "$MSSID" | awk -F "," '{print $2}')
-
-	#echo "$MSSID"
-	#echo "$MPASS"
-
-	# If the user entered a manual password, then use the password nmcli command
-	if [ "$MPASS" = "" ]; then
-		nmcli dev wifi con "$MSSID"
-	else
-		nmcli dev wifi con "$MSSID" password "$MPASS"
-	fi
-
-elif [ "$CHENTRY" = "toggle on" ]; then
-	nmcli radio wifi on
-
-elif [ "$CHENTRY" = "toggle off" ]; then
-	nmcli radio wifi off
-
+if [ -z "$chosen_row" ]; then exit
+elif [ "$chosen_row" = "$enable_row" ]; then
+  nmcli radio wifi on
+elif [ "$chosen_row" = "$disable_row" ]; then
+  nmcli radio wifi off
 else
-
-	# If the connection is already in use, then this will still be able to get the SSID
-	if [ "$CHSSID" = "*" ]; then
-		CHSSID=$(echo "$CHENTRY" | sed  's/\s\{2,\}/\|/g' | awk -F "|" '{print $3}')
-	fi
-
-	# Parses the list of preconfigured connections to see if it already contains the chosen SSID. This speeds up the connection process
-	if [[ $(echo "$KNOWNCON" | grep "$CHSSID") = "$CHSSID" ]]; then
-		nmcli con up "$CHSSID"
-	else
-		if [[ "$CHENTRY" =~ "WPA2" ]] || [[ "$CHENTRY" =~ "WEP" ]]; then
-			WIFIPASS=$(echo "if connection is stored, hit enter" | rofi -dmenu -p "password: " -lines 1 -font "$FONT" )
-		fi
-		nmcli dev wifi con "$CHSSID" password "$WIFIPASS"
-	fi
-
+  # Parses the list of known connections to see if it already contains the
+  # chosen SSID.
+  # nmcli connection show -- more detailed list of saved networks
+  if [[ ! -z $(nmcli -g NAME connection | grep -wx "$chosen_ssid") ]]; then
+    nmcli connection up id "$chosen_ssid" --ask | notify_success
+  else
+    [[ "$chosen_row" =~ "WPA2" ]] &&
+      wifi_pass=$(rofi -dmenu -password  \
+        -theme-str '#entry { placeholder: "password .."; }')
+    nmcli device wifi connect "$chosen_ssid" password "$wifi_pass" |
+      notify_success
+  fi
 fi
