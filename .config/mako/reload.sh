72e4b450b92a36b76f759d23508f239d9e628ffc
earthian <d3v1ant@mail.ru>
2023-02-21 21:06:36 +0300

    mako notifications test script change

--- .config/mako/reload.sh
+++ .config/mako/reload.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 
+# '-i firefox' to test notification with an icon
 makoctl reload
 # to test 'high' urgency notificatoin add '-u critical '
-notify-send -a "Test noteif app" -i firefox -t 5000 "Here is some summary" "needed to <s>create</s> that script cuz /usr/bin/makoctl reload wasn't working and was preventing the notification to appear with no logs"
+notify-send -a "Notification test" -t 5000 "<summary line>" "Here is an example of how single (not grouped) notification will look with these theme colors"
 # grouped
-notify-send -a "Test notif app" -i firefox -t 5000 "Here is some summary" "needed to <s>create</s> that script cuz /usr/bin/makoctl reload wasn't working and was preventing the notification to appear with no logs"
-notify-send -a "Test notif app" -i firefox -t 5000 "Here is some summary" "needed to <s>create</s> that script cuz /usr/bin/makoctl reload wasn't working and was preventing the notification to appear with no logs"
-notify-send -a "Test notif app" -i firefox -t 5000 "Here is some summary" "needed to <s>create</s> that script cuz /usr/bin/makoctl reload wasn't working and was preventing the notification to appear with no logs"
+# notify-send -a "Grouped notification test" -t 5000 "<grouped summary line>" "Here is an example of how grouped notification will look with these theme colors"
+# notify-send -a "Grouped notification test" -t 5000 "<grouped summary line>" "Here is an example of how grouped notification will look with these theme colors"
+# notify-send -a "Grouped notification test" -t 5000 "<grouped summary line>" "Here is an example of how grouped notification will look with these theme colors"
