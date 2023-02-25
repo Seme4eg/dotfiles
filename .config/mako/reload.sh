65d0f33ede81bec0bb96ac00b4c250c8bad4d261
earthian <d3v1ant@mail.ru>
2023-02-26 00:23:25 +0300

    mako reload script insignificance

--- .config/mako/reload.sh
+++ .config/mako/reload.sh
@@ -3,7 +3,7 @@
 # '-i firefox' to test notification with an icon
 makoctl reload
 # to test 'high' urgency notificatoin add '-u critical '
-notify-send -a "Notification test" -t 5000 "<summary line>" "Here is an example of how single (not grouped) notification will look with these theme colors"
+notify-send -a "Theme changed" -t 5000 "<summary line>" "Here is an example of how single (not grouped) notification will look with these theme colors"
 # grouped
 # notify-send -a "Grouped notification test" -t 5000 "<grouped summary line>" "Here is an example of how grouped notification will look with these theme colors"
 # notify-send -a "Grouped notification test" -t 5000 "<grouped summary line>" "Here is an example of how grouped notification will look with these theme colors"
