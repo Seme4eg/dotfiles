be1761b5bf638cb02c06785f09b05f57915a937f
earthian <d3v1ant@mail.ru>
2023-04-17 11:55:57 +0400

    git update-index aliases

--- .config/zsh/aliases/aliases.zsh
+++ .config/zsh/aliases/aliases.zsh
@@ -7,9 +7,13 @@ alias cat="bat"
 # - https://github.com/PhrozenByte/rmtrash
 alias rm="rip -i" # https://github.com/nivekuil/rip
 alias du="ncdu" # get to know your storage (https://github.com/rofl0r/ncdu)
+alias jq="gojq"
 
 # du -h --max-depth=1 ~/ | sort -h # make an alias?
 
 alias vedroid="waydroid session stop && waydroid session start &"
 alias grubconf="sudo grub-mkconfig -o /boot/grub/grub.cfg"
 alias 0x0="curl -F 'file=@-' 0x0.st" # < file
+
+alias gau="git update-index --assume-unchanged"
+alias gnau="git update-index --no-assume-unchanged"
