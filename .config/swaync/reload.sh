76bdbc4f495ba22ebd3d803c69c1873aacfc8494
earthian <d3v1ant@mail.ru>
2023-05-04 15:28:31 +0400

    add example notifications to swaync reload script

--- .config/swaync/reload.sh
+++ .config/swaync/reload.sh
@@ -7,4 +7,6 @@ swaync-client -R
 
 # -u critical
 # -t 5000
-# notify-send -i webcord "Test notification" "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut mauris quis ante bibendum gravida. Sed mollis neque felis, quis rutrum risus porttitor vel. Duis finibus lorem nec massa lacinia, quis vehicula est ultricies. Suspendisse consectetur orci a quam fringilla, id malesuada quam posuere. Vestibulum non ipsum consequat, venenatis turpis nec, viverra mi. Sed bibendum odio vel augue luctus congue."
+# notify-send -i webcord "Test icon notification" "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut mauris quis ante bibendum gravida. Sed mollis neque felis, quis rutrum risus porttitor vel. Duis finibus lorem nec massa lacinia, quis vehicula est ultricies. Suspendisse consectetur orci a quam fringilla, id malesuada quam posuere. Vestibulum non ipsum consequat, venenatis turpis nec, viverra mi. Sed bibendum odio vel augue luctus congue."
+# notify-send "Test noicon notification" "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut mauris quis ante bibendum ..."
+# notify-send -u critical "Test critical notification" "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut mauris quis ante bibendum ..."
