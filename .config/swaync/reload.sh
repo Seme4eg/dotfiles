b3ffd29059c4f0534e92c98742b7098368129c73
earthian <d3v1ant@mail.ru>
2023-05-04 11:58:48 +0400

    remove dnd support (ain't using)
    previous commit is just for history, just in case i'll ever need it

--- .config/swaync/reload.sh
+++ .config/swaync/reload.sh
@@ -7,4 +7,4 @@ swaync-client -R
 
 # -u critical
 # -t 5000
-# notify-send -i telegram "Test notification" "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut mauris quis ante bibendum gravida. Sed mollis neque felis, quis rutrum risus porttitor vel. Duis finibus lorem nec massa lacinia, quis vehicula est ultricies. Suspendisse consectetur orci a quam fringilla, id malesuada quam posuere. Vestibulum non ipsum consequat, venenatis turpis nec, viverra mi. Sed bibendum odio vel augue luctus congue."
+# notify-send -i webcord "Test notification" "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut mauris quis ante bibendum gravida. Sed mollis neque felis, quis rutrum risus porttitor vel. Duis finibus lorem nec massa lacinia, quis vehicula est ultricies. Suspendisse consectetur orci a quam fringilla, id malesuada quam posuere. Vestibulum non ipsum consequat, venenatis turpis nec, viverra mi. Sed bibendum odio vel augue luctus congue."
