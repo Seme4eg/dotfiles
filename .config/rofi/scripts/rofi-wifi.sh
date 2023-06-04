2f22bed6717175fd2ac9b5f880b099af18e15d26
earthian <d3v1ant@mail.ru>
2023-06-04 17:50:34 +0300

    wifi script crutch removed

--- .config/rofi/scripts/rofi-wifi.sh
+++ .config/rofi/scripts/rofi-wifi.sh
@@ -1,35 +1,11 @@
 #!/usr/bin/env bash
 
-FIELDS=IN-USE,SSID,SECURITY,FREQ,RATE,SIGNAL,BARS # ACTIVE
-
+# IN-USE, BSSID
+FIELDS=ACTIVE,SSID,SECURITY,FREQ,RATE,SIGNAL
 # awk NF to filter empty lines from output
-networks=$(nmcli --fields "$FIELDS" device wifi list | sed '/--/d' | awk NF)
-# get ssids list, -t flag returns list without 1st 'header' line
-ssids=$(nmcli -t --fields SSID device wifi list | awk NF)
+networks=$(nmcli -f "$FIELDS" device wifi list | sed '/--/d' | awk NF)
 # will b empty if no active connection
 initial_ssid=$(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -d: -f2)
-
-# Getting ssid of the chosen row by mapping list or rows and list of ssids to
-# each other. Reason for this function is that i didn't find another way to
-# parse row to get it's ssid. Because of possible spaces in ssids.
-get_ssid() {
-  # start with 0 index cuz we are also iterating over 'headers' line first
-  index=0
-
-  echo "$networks" |
-    while read row; do
-      # get 'index'th line from the list of ssids
-      ssid=$(echo "$ssids" | awk NR==$index)
-
-      # remove all spaces from both strings, maybe not best way to do it idk
-      if [ ${row// /} = ${1// /} ]; then
-        echo "$ssid"
-        break
-      fi
-
-      (( index++ ))
-    done
-}
 
 wifi_on() {
   if [ $(nmcli radio wifi) = "enabled" ]; then
@@ -84,7 +60,7 @@ forget_ssid() {
 
 toggle_ssid() {
   ssid="$1"
-  vpnstatus=`systemctl is-active openvpn-client@client.service`
+  vpnstatus=$(systemctl is-active openvpn-client@client.service)
   if [ $vpnstatus == 'active' ]; then
     sudo systemctl stop openvpn-client@client.service
     vpnneedsactivation=0
@@ -112,7 +88,7 @@ toggle_ssid() {
   }
 
   _connect_protected() {
-    wifi_pass=$(rofi -dmenu -password  \
+    wifi_pass=$(rofi -dmenu -password \
       -theme-str '#entry { placeholder: "password .."; }')
     # if no password provided - quit that script and restore initial connection
     # maybe show return to main menu instead?
@@ -163,7 +139,7 @@ ssid_menu() {
   connected=$(ssid_connected "$ssid")
   saved=$(ssid_saved "$ssid")
   ssid_saved "$ssid" && options="$connected\n$saved\n$goback" ||
-      options="$connected\n$goback"
+    options="$connected\n$goback"
 
   # Open rofi menu, read chosen option
   chosen="$(echo -e "$options" | rofi -dmenu -i \
@@ -192,8 +168,8 @@ function update_networks() {
   while true; do
     sleep 1
     _networks=$(nmcli --fields "$FIELDS" device wifi list | sed '/--/d' | awk NF)
-    _count=`echo "$_networks" | wc -l`
-    count=`echo "$networks" | wc -l`
+    _count=$(echo "$_networks" | wc -l)
+    count=$(echo "$networks" | wc -l)
 
     # -w for 'wait' to not spam tnose notification until user refreshes
     [ $_count -ne $count ] && say -w -e "Networks changed, refresh"
@@ -204,30 +180,30 @@ show_menu() {
   state=$(wifi_on)
 
   # start background networks list updating, and kill it later by pid
-  update_networks & update_pid=$!
+  update_networks &
+  update_pid=$!
 
   wifi_on && options="$state\n$networks" || options="$state"
   # force monospace font to not get those fields messy
-  chosen_row=$(echo -e "$options\nrefresh" | uniq -u |
-                 rofi -dmenu -i -selected-row 2 \
-                   -theme-str '#entry { placeholder: "Wi-Fi SSID:"; }' \
-                   -theme-str '* { font: "syne mono 13"; }')
+  option=$(echo -e "$options\nrefresh" | uniq -u |
+    rofi -dmenu -i -selected-row 2 \
+      -theme-str '#entry { placeholder: "Wi-Fi SSID:"; }' \
+      -theme-str '* { font: "syne mono 13"; }')
 
   kill $update_pid
 
-  # chosen_ssid might b empty in case we chose 'enable/disable wifi' row
+  # might b empty in case we chose 'enable/disable wifi' row
   # so we don't check for it's emptyness
-  if [ -z "$chosen_row" ]; then exit; fi
+  [ -z "$option" ] && exit 1
 
-  ssid=$(get_ssid "$chosen_row")
-
-  if [ "$chosen_row" = "$state" ]; then
+  if [ "$option" = "$state" ]; then
     toggle_wifi
-  elif [ "$chosen_row" = 'refresh' ]; then
+  elif [ "$option" = 'refresh' ]; then
     networks=$(nmcli --fields "$FIELDS" device wifi list | sed '/--/d' | awk NF)
-    ssids=$(nmcli -t --fields SSID device wifi list | awk NF)
     show_menu
   else
+    ssid=$(echo "$option" | awk -F '  +' '{print $2}')
+    echo "$ssid"
     ssid_menu "$ssid"
   fi
 }
