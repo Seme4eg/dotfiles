e286a98a3ec90046402922e8e3fc14e66390eeaf
earthian <d3v1ant@mail.ru>
2023-06-21 20:38:50 +0800

    pictures folder reorder + new one

--- .config/swaync/reload.sh
+++ .config/swaync/reload.sh
@@ -15,4 +15,4 @@ swaync-client -R
 # notify-send -i $(rngicon) "Test icon notification" "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut mauris quis ante bibendum gravida. Sed mollis neque felis, quis rutrum risus porttitor vel. Duis finibus lorem nec massa lacinia, quis vehicula est ultricies. Suspendisse consectetur orci a quam fringilla, id malesuada quam posuere. Vestibulum non ipsum consequat, venenatis turpis nec, viverra mi. Sed bibendum odio vel augue luctus congue."
 # notify-send "Test noicon notification" "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut mauris quis ante bibendum ..."
 # notify-send -u critical "Test critical notification" "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut mauris quis ante bibendum ..."
-# notify-send -i /home/earthian/Pictures/Avbl/file004.jpg hi "<img src=\"/home/earthian/Pictures/wps/pc/wadim_kashin/tourism_part_two___march_work_by_solarsouth_d8lz56y.jpg\"/> hi there"
+# notify-send -i /home/earthian/Pictures/DCIM/Avbl/file004.jpg hi "<img src=\"/home/earthian/Pictures/wps/pc/wadim_kashin/tourism_part_two___march_work_by_solarsouth_d8lz56y.jpg\"/> hi there"
