af5316f270b3e74dfcbaa9fdcceca9c98067b2c8
earthian <d3v1ant@mail.ru>
2023-08-29 13:53:56 +0700

    change yay update alias to update only aur pkgs

--- .config/zsh/aliases/yay-aliases.zsh
+++ .config/zsh/aliases/yay-aliases.zsh
@@ -1,11 +1,11 @@
 #!/usr/bin/env zsh
 
-alias yi="yay -S"    # install
-alias yr="yay -Rs"   # remove
-alias ysr="yay -Ss"  # search remotes
-alias ysl="yay -Qs"  # search locally
-alias ys="yay -Syy"  # sync
-alias yu="yay -Syyu" # update
+alias yi="yay -S"     # install
+alias yr="yay -Rs"    # remove
+alias ysr="yay -Ss"   # search remotes
+alias ysl="yay -Qs"   # search locally
+alias ys="yay -Syy"   # sync
+alias yu="yay -Sayyu" # update only AUR packages
 # -Scc is more agressive approach, read bout it before using in case u decide to
 alias ycc="yay -Sc" # Clear Cache
 # force a refresh of the package cache used for completions if it broke after
