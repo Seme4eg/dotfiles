93f560238209be3744b711c69073b5317b8d9056
earthian <418@duck.com>
2024-05-26 23:46:01 +0700

    add some podman aliases

--- .config/zsh/aliases/podman.zsh
+++ .config/zsh/aliases/podman.zsh
@@ -1,6 +1,10 @@
 #!/usr/bin/env zsh
 
 alias pd="podman"
-alias pdrun="pd run --replace --name postgres -p 5432:5432 -d -e POSTGRES_PASSWORD=test postgres:latest"
+alias pdcreate="pd run --replace --name postgres -p 5432:5432 -d \
+-e POSTGRES_USER=user -e POSTGRES_PASSWORD=admin -e POSTGRES_HOST_AUTH_METHOD=trust \
+postgres:latest"
 alias pdrm="pd rm -f"
 alias pdup="pd up -d"
+alias pdexec="pd exec -it " # add -U <username>
+alias pdlogs="pd logs"      # and <name> of container
