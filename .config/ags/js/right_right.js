70eb3866d00c20afbb034e2c53b27eff15b639c3
earthian <418@duck.com>
2024-06-26 17:41:16 +0700

    ags zram simple widget

--- .config/ags/js/right_right.js
+++ .config/ags/js/right_right.js
@@ -8,8 +8,6 @@ export default function RRight() {
   });
 }
 
-const divide = ([total, free]) => free / total;
-
 function CPU() {
   const historyLen = 17;
 
@@ -80,18 +78,34 @@ function CPU() {
 }
 
 function Memory() {
+  return Widget.Box({
+    className: "memory",
+    spacing: 7,
+    children: [Ram(), Swap()],
+  });
+}
+
+function Ram() {
+  // @what either 'Mem' or 'Swap', @from is the output of 'free' command
+  const get = (what, from) =>
+    from
+      .split("\n")
+      .find((line) => line.includes(what + ":"))
+      .split(/\s+/);
+
   const ram = Variable(0, {
     poll: [
       2000,
       "free",
-      (out) =>
-        divide(
-          out
-            .split("\n")
-            .find((line) => line.includes("Mem:"))
-            .split(/\s+/)
-            .splice(1, 2),
-        ),
+      (out) => {
+        let ram = get("Mem", out).splice(1, 2); // returns total and used respectively
+        let swap = get("Swap", out).splice(1, 1); // returns total swap amount
+
+        // since i am using zram, which takes part of ram
+        let ramTotal = ram[0] - swap;
+
+        return ram[1] / ramTotal; // divide used by total, excluding zram
+      },
     ],
   });
 
@@ -106,6 +120,28 @@ function Memory() {
         label: "󰍛",
       }),
     ],
+  });
+}
+
+function Swap() {
+  const swap = Variable(0, {
+    poll: [
+      2000,
+      // can't use 'free' command for swap info since it doesn't consider
+      // compression
+      "zramctl --raw --noheadings --bytes zram0",
+      (out) => {
+        // Array:  DISKSIZE   DATA COMPR  TOTAL STREAMS MOUNTPOINT
+        let swapInfo = out.split(/\s+/).splice(2);
+        return swapInfo[3] / swapInfo[0]; // total used / disksize
+      },
+    ],
+  });
+
+  return Widget.ProgressBar({
+    vertical: true,
+    inverted: true,
+    value: swap.bind(),
   });
 }
 
