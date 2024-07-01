44b80a62cf974581a00ba713e51e4437f71dfb1d
earthian <418@duck.com>
2024-07-01 17:54:57 +0700

    remove syncing before installing single package

--- .config/zsh/aliases/pacman-aliases.zsh
+++ .config/zsh/aliases/pacman-aliases.zsh
@@ -1,11 +1,11 @@
 #!/usr/bin/env zsh
 
 alias pm="sudo pacman"
-alias pmi="pms && pm -S" # install
-alias pmr="pm -Rs"       # remove
-alias pmsr="pm -Ss"      # search remotes
-alias pmsl="pm -Qs"      # search locally
-alias pml="pm -Ql"       # list package
+alias pmi="pm -S"   # install
+alias pmr="pm -Rs"  # remove
+alias pmsr="pm -Ss" # search remotes
+alias pmsl="pm -Qs" # search locally
+alias pml="pm -Ql"  # list package
 # If no orphans were found, the output is error: argument '-' specified with
 # empty stdin. This is expected as no arguments were passed to pacman -Rns.
 alias pmro="pacman -Qtdq | sudo pacman -Rns -" # remove orphaned
