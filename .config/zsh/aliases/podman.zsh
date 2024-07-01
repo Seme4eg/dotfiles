d474db3f3adbfcc3471efb0227f8fdfe0901dc9f
earthian <418@duck.com>
2024-07-02 00:37:41 +0700

    podman alias

--- .config/zsh/aliases/podman.zsh
+++ .config/zsh/aliases/podman.zsh
@@ -1,9 +1,12 @@
 #!/usr/bin/env zsh
 
 alias pd="podman"
-alias pdcreate="pd run --replace --name postgres -p 5432:5432 -d \
--e POSTGRES_USER=user -e POSTGRES_PASSWORD=admin -e POSTGRES_HOST_AUTH_METHOD=trust \
-postgres:latest"
+alias pdpostgres="pd run --replace --name postgres -p 5432:5432 -d \
+  -e POSTGRES_USER=user -e POSTGRES_PASSWORD=admin \
+  -e POSTGRES_HOST_AUTH_METHOD=trust \
+  postgres:latest"
+alias pdrabbitmq="pd run --replace --name rabbitmq -p 5672:5672 -p 15672:15672 \
+  docker.io/rabbitmq:latest"
 alias pdrm="pd rm -f"
 alias pdup="pd up -d"
 alias pdexec="pd exec -it " # add -U <username>
