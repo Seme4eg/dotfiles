6c608a69f67fd82cb3331af57b9c7f997fd2ddb7
earthian <418@duck.com>
2024-08-08 15:59:37 +0700

    pmI alias pacman

--- .config/zsh/aliases/pacman-aliases.zsh
+++ .config/zsh/aliases/pacman-aliases.zsh
@@ -1,11 +1,12 @@
 #!/usr/bin/env zsh
 
 alias pm="sudo pacman"
-alias pmi="pms && pm -S" # install
-alias pmr="pm -Rs"       # remove
-alias pmsr="pm -Ss"      # search remotes
-alias pmsl="pm -Qs"      # search locally
-alias pml="pm -Ql"       # list package
+alias pmi="pm -Sy && pm -S" # install
+alias pmI="pm -S"           # install without package syncing
+alias pmr="pm -Rs"          # remove
+alias pmsr="pm -Ss"         # search remotes
+alias pmsl="pm -Qs"         # search locally
+alias pml="pm -Ql"          # list package
 # If no orphans were found, the output is error: argument '-' specified with
 # empty stdin. This is expected as no arguments were passed to pacman -Rns.
 alias pmro="pacman -Qtdq | sudo pacman -Rns -" # remove orphaned
