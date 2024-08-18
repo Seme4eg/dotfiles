af4f58366c319b124fe88766ce52b71a9a936fc6
earthian <418@duck.com>
2024-08-18 13:36:19 +0700

    ys alias also fixes cache

--- .config/zsh/aliases/yay-aliases.zsh
+++ .config/zsh/aliases/yay-aliases.zsh
@@ -1,13 +1,13 @@
 #!/usr/bin/env zsh
 
-alias yi="yay -S"     # install
-alias yr="yay -Rs"    # remove
-alias ysr="yay -Ss"   # search remotes
-alias ysl="yay -Qs"   # search locally
-alias ys="yay -Syy"   # sync
-alias yu="yay -Sayyu" # update only AUR packages
+alias yi="yay -S"                          # install
+alias yr="yay -Rs"                         # remove
+alias ysr="yay -Ss"                        # search remotes
+alias ysl="yay -Qs"                        # search locally
+alias yfc="yay -Pcc"                       # yay fix cache
+alias ys="yay -Syy && yay -Pcc >/dev/null" # sync and fix cache
+alias yu="yay -Sayyu"                      # update only AUR packages
 # -Scc is more agressive approach, read bout it before using in case u decide to
 alias ycc="yay -Sc" # Clear Cache
 # force a refresh of the package cache used for completions if it broke after
 # above command
-alias yfc="yay -Pcc" # yay fix cache
