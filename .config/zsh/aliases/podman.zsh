d7458c089fbc91015075365b04c3b23cf554b811
earthian <418@duck.com>
2024-09-25 13:44:50 +0700

    feat: pdredis zsh alias

--- .config/zsh/aliases/podman.zsh
+++ .config/zsh/aliases/podman.zsh
@@ -1,12 +1,14 @@
 #!/usr/bin/env zsh
 
 alias pd="podman"
-alias pdpostgres="pd run --replace --name postgres -p 5432:5432 -d \
+alias pdpostgres="pd run -d --replace --name postgres -p 5432:5432 \
   -e POSTGRES_USER=user -e POSTGRES_PASSWORD=admin \
   -e POSTGRES_HOST_AUTH_METHOD=trust \
-  postgres:latest"
-alias pdrabbitmq="pd run --replace --name rabbitmq -p 5672:5672 -p 15672:15672 \
-  docker.io/rabbitmq:latest"
+  postgres:17rc1-alpine3.20"
+alias pdrabbitmq="pd run -d --replace --name rabbitmq -p 5672:5672 -p 15672:15672 \
+  docker.io/rabbitmq:4.0.0-alpine"
+alias pdredis="pd run -d --replace --name rabbitmq -p 6379:6379 \
+  docker.io/redis:7.4.0-alpine"
 alias pdrm="pd rm -f"
 alias pdup="pd up -d"
 alias pdexec="pd exec -it " # add -U <username>
