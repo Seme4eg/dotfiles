ff3452c4e00537d38254e962acbd7040c77ac3bc
earthian <418@duck.com>
2024-12-10 18:56:37 +0700

    fix podman postgres url

--- .config/zsh/aliases/podman.zsh
+++ .config/zsh/aliases/podman.zsh
@@ -4,7 +4,7 @@ alias pd="podman"
 alias pdpostgres="pd run -d --replace --name postgres -p 5432:5432 \
   -e POSTGRES_USER=user -e POSTGRES_PASSWORD=admin \
   -e POSTGRES_HOST_AUTH_METHOD=trust \
-  postgres:17rc1-alpine3.20"
+  docker.io/postgres:alpine"
 alias pdrabbitmq="pd run -d --replace --name rabbitmq -p 5672:5672 -p 15672:15672 \
   docker.io/rabbitmq:4.0.0-alpine"
 alias pdredis="pd run -d --replace --name rabbitmq -p 6379:6379 \
