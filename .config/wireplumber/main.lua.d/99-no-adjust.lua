1dffdb49bd57418a712681eca9721f117661c6e5
earthian <418@duck.com>
2025-01-03 18:07:33 +0700

    clean up discord alternatives from system

--- .config/wireplumber/main.lua.d/99-no-adjust.lua
+++ .config/wireplumber/main.lua.d/99-no-adjust.lua
@@ -2,11 +2,8 @@ table.insert(default_access.rules, {
    matches = {
    {
      { "application.process.binary", "=", "electron" },
-     { "application.process.binary", "=", "webcord" },
      { "application.process.binary", "=", "firefox" }
-     { "application.process.binary", "=", "legcord" }
      { "application.process.binary", "=", "vesktop" }
-     { "application.process.binary", "=", "abaddon" }
    }
    },
    default_permissions = "rx",
