826c9ee5a8f6cafefc429dfa5a342cd0c7fd8206
earthian <418@duck.com>
2025-01-11 15:39:18 +0700

    astal take 2

--- .config/astal/init.lua
+++ .config/astal/init.lua
@@ -1,10 +1,32 @@
+local astal = require("astal")
 local App = require("astal.gtk3.app")
+local GLib = require("lgi").require("GLib", "2.0")
 local Widget = require("astal.gtk3.widget")
 local Anchor = require("astal.gtk3").Astal.WindowAnchor
 local Variable = require("astal").Variable
 
+-- pcall(require, "luarocks.loader") -- XXX
+
+-- local icons = config_dir .. "/icons" XXX
+
+local astal_styles_dir = GLib.getenv("HOME") .. "/.config/astal/styles"
+local scss = astal_styles_dir .. "/main.scss"
+local css = GLib.getenv("HOME") .. "/.cache/astal/astal.css"
+local function reloadCSS()
+  astal.exec(string.format("sass --no-source-map %s %s", scss, css))
+  App:reset_css()
+  App:apply_css(css)
+end
+
+-- XXX: ?in case you will need to reload styles manually - run 'agsv1 -r "reloadCSS()"'
+-- from terminal, after you uncomment line below.
+-- globalThis.reloadCSS = reloadCSS;
+astal.monitor_file(GLib.getenv("HOME") .. "/.cache/wal/colors.scss", reloadCSS)
+astal.monitor_file(astal_styles_dir, reloadCSS)
+
+-- local Left = require("widget.Left")
 local Center = require("widget.Center")
-local Left = require("widget.Left")
+-- local Right = require("widget.Right")
 
 local theme = Variable("dark")
 -- globalThis.theme = theme; -- XXX
@@ -22,19 +44,16 @@ function Bar(monitor)
       -- start_widget: Left(),
       -- center_widget: Center(),
       -- end_widget: Right(),
-      start_widget = Left(),
-      -- center_widget = Center(),
-      center_widget = Widget.Box(), --core dumps every fucking where
-      --fuck this shit im out, tf pushing such unstable shit to public
+      start_widget = Widget.Box(),
+      center_widget = Center(),
       end_widget = Widget.Box()
     }),
   })
 end
 
 App:start({
+  css = css,
   main = function()
-    -- you will instantiate Widgets here
-    -- and setup anything else if you need
     Bar(0)
   end,
 })
--- .config/astal/widget/Center.lua
+++ .config/astal/widget/Center.lua
@@ -1,39 +1,81 @@
 local Variable = require("astal").Variable
 -- local GLib = require("GLib")
+local GLib = require("lgi").require("GLib", "2.0")
 local Widget = require("astal.gtk3.widget")
-local json = require("lib.json")
 
-local date = Variable("{}")
-    :poll(1000, { "bash", "-c", "date +'{\"count\":\"%j\",\"hour\":\"%H\",\"minute\":\"%M\",\"day\":\"%a %d/%m\"}'" })
+-- local date = Variable("{}")
+--     :poll(1000, { "bash", "-c", "date +'{\"count\":\"%j\",\"hour\":\"%H\",\"minute\":\"%M\",\"day\":\"%a %d/%m\"}'" })
 -- :poll(1000, { [["date +'{"count":"%j","hour":"%H","minute":"%M","day":"%a %d/%m"}'"]] })
 
 -- local function Clock(format)
--- 	local time = Variable(""):poll(1000, function()
--- 		return GLib.DateTime.new_now_local():format(format)
--- 	end)
 
 -- 	return Widget.Label({
 -- 		class_name = "Time",
--- 		on_destroy = function()
--- 			time:drop()
--- 		end,
 -- 		label = time(),
 -- 	})
 -- end
 
 function Clock()
-  -- return Widget.Overlay({
-  return Widget.Label({
-    class_name = "fg",
-    css = "color: transparent;",
-    label = date(function(str)
-      print("DATE" .. date:get())
-      local data = json.decode(str)
-      return string.format("%s %s", data.count, data.day)
-    end)
+  local time = Variable(""):poll(1000, function()
+    return GLib.DateTime.new_now_local()
+  end)
+
+  local day_format = "#%j %a %d/%m"
+
+  return Widget.Overlay({
+    -- FIXME: didn't figure out how to bring child element on top of the overlay
+    -- As a hack - duplikating element to get width set and make it transparent.
+    child = Widget.Label({
+      class_name = "fg",
+      css = "color: transparent;",
+      on_destroy = function()
+        time:drop()
+      end,
+      label = time(function(date)
+        return date:format(day_format)
+      end)
+    }),
+    overlays = {
+      Widget.Box({
+        halign = "CENTER",
+        class_name = "bg",
+        Widget.CenterBox({
+          Widget.Label({
+            class_name = "hour",
+            label = time(function(date) return date:format("%H") end)
+          }),
+          Widget.Label({
+            class_name = "colon",
+            label = ":"
+          }),
+          Widget.Label({
+            class_name = "minute",
+            label = time(function(date) return date:format("%M") end)
+          }),
+        }),
+      }),
+      Widget.Label({
+        class_name = "fg",
+        label = time(function(date) return date:format(day_format) end)
+      }),
+    }
   })
-  -- })
 end
+
+local function Time(format)
+  local time = Variable(""):poll(1000, function()
+    return GLib.DateTime.new_now_local():format(format)
+  end)
+
+  return Widget.Label({
+    class_name = "Time",
+    on_destroy = function()
+      time:drop()
+    end,
+    label = time(),
+  })
+end
+
 
 return function()
   return Widget.Box({
