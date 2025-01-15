0a660eb508cb451521520cd2e72c17cceebb7ec9
earthian <418@duck.com>
2025-01-15 12:53:29 +0700

    emacs pywal template

---
+++ .config/wal/templates/palette.el
@@ -0,0 +1,15 @@
+
+(setq ewal-base-palette
+      '((comment    . "{color8}") ;; unofficial pywal comment color
+        (background . "{background}")
+        (foreground . "{foreground}")
+        (cursor     . "{cursor}")
+        ;; these 8 are tty colors that can be found in 'tty-defined-color-alist'
+        (black      . "{background}")
+        (red        . "{color1}")
+        (green      . "{color2}")
+        (yellow     . "{color3}")
+        (blue       . "{color4}")
+        (magenta    . "{color5}")
+        (cyan       . "{color6}")
+        (white      . "{color7}")))
