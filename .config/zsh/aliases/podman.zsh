13ae315b57e735b198737da1e6b7cbb190d70ddc
earthian <418@duck.com>
2025-02-11 14:43:54 +0400

    podman aliases

--- .config/zsh/aliases/podman.zsh
+++ .config/zsh/aliases/podman.zsh
@@ -2,9 +2,10 @@
 
 alias pd="podman"
 alias pdpostgres="pd run -d --replace --name postgres -p 5432:5432 \
-  -e POSTGRES_USER=user -e POSTGRES_PASSWORD=admin \
+  -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=admin \
   -e POSTGRES_HOST_AUTH_METHOD=trust \
-  docker.io/postgres:alpine"
+  docker.io/postgres:13-alpine3.20" # or just alpine, but need v13 for work
+alias applyddl="psql -U admin -h localhost -p 5432 -d postgres -f"
 alias pdrabbitmq="pd run -d --replace --name rabbitmq -p 5672:5672 -p 15672:15672 \
   docker.io/rabbitmq:4.0.0-alpine"
 alias pdredis="pd run -d --replace --name rabbitmq -p 6379:6379 \
