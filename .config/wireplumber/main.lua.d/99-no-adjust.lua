67fe4e420156f5b905ce2fc4cc657e4f6065a5f7
earthian <418@duck.com>
2025-05-17 02:53:56 +0800

    document on how to find binaries name for wp

--- .config/wireplumber/main.lua.d/99-no-adjust.lua
+++ .config/wireplumber/main.lua.d/99-no-adjust.lua
@@ -1,3 +1,8 @@
+-- to figure out the binary process name:
+-- pw-cli list-objects
+-- Look for the client ID that corresponds to the application you need
+-- pw-cli info <client-id> <- and here you will find the binary stat
+
 table.insert(default_access.rules, {
   matches = {
     {
