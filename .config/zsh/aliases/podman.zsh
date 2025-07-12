5898c7997a2b5d1f7a1bc84288485a4857eb5b3e
earthian <418@duck.com>
2025-07-12 19:26:51 +0300

    zsh fix typo in redis alias

--- .config/zsh/aliases/podman.zsh
+++ .config/zsh/aliases/podman.zsh
@@ -8,7 +8,7 @@ alias pdpostgres="pd run -d --replace --name postgres -p 5432:5432 \
 alias applyddl="psql -U admin -h localhost -p 5432 -d postgres -f"
 alias pdrabbitmq="pd run -d --replace --name rabbitmq -p 5672:5672 -p 15672:15672 \
   docker.io/rabbitmq:4.0.0-alpine"
-alias pdredis="pd run -d --replace --name rabbitmq -p 6379:6379 \
+alias pdredis="pd run -d --replace --name redis -p 6379:6379 \
   docker.io/redis:7.4.0-alpine"
 alias pdrm="pd rm -f"
 alias pdup="pd up -d"
