bb1257b0ad6e926384dfafe31882a855d9735310
earthian <418@duck.com>
2025-07-22 17:15:02 +0300

    add java_options default xdg location
    keep home dir clean

--- .config/zsh/config.zsh
+++ .config/zsh/config.zsh
@@ -57,6 +57,7 @@ export PNPM_HOME=$XDG_DATA_HOME/pnpm
 export W3M_DIR="$XDG_STATE_HOME/w3m"
 export PARALLEL_HOME="$XDG_CONFIG_HOME"/parallel
 export PSQL_HISTORY="$XDG_STATE_HOME/psql_history"
+export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
 
 # ---
 
