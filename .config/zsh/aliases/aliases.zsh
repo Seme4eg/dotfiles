54bbd9208bfc98f35b2552066481bdded723410b
earthian <418@duck.com>
2025-08-27 12:57:32 +0300

    clicker setup

--- .config/zsh/aliases/aliases.zsh
+++ .config/zsh/aliases/aliases.zsh
@@ -26,6 +26,10 @@ alias cheatengine="xhost +local: &; gameconqueror"
 # NOTE: tlp-stat also shows the capacity
 alias batinfo="upower -i /org/freedesktop/UPower/devices/battery_BAT1"
 
+# bind = SUPERCTRL, W, exec, pkill ydotool || ydotoold & { sleep 0.4; ydotool click -r 3000 0xC0; }
+# alias clicker="ydotoold &; sleep 0.4; ydotool mousemove --absolute 300 250; ydotool click -r 3000 0xC0"
+# clicker() { ydotoold &; sleep 0.4; ydotool click -r 3000 0xC0; }
+
 hyprupd() {
   # NOTE: need manuall update of all these
   # secondary packages, need to build first
