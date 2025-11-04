4a7a0b8ddf9a487f3817cc3f20d7ec297d7fe753
earthian <418@duck.com>
2025-11-04 23:05:54 +0200

    rsync archive done and working

--- .config/zsh/aliases/aliases.zsh
+++ .config/zsh/aliases/aliases.zsh
@@ -28,7 +28,15 @@ alias mttorrent="sudo mount -t nfs -o vers=4 192.168.1.81:/home/earthian/data/to
 
 alias homediff="diff <(tree -L 1 -a --dirsfirst) dotfiles/.local/share/HOME"
 
-alias rsync_archive='sudo rsync -rP --delete ~/mem-arch/' # .. and append destination
+# If at home replace tailscale ip with this one: earthian@192.168.1.81:~/homelab
+# - before '--archive' flag i had '--recursive --times'
+# - if adding '--info=progress2' add ONLY this info flag as mixed with any other
+#   ones it turns to be a mess
+# - if just want to see more info add --verbove (increases verbocity by one) and
+#   it is essentially the '--info=' flag with sane params for all the info you
+#   need. Run `rsync --info=help' to see all flags and levels`
+alias rsync_archive='rsync --archive --info=progress2 --exclude=.stversions --delete ~/mem-arch/ earthian@100.95.49.75:~/archive'
+alias rsync_archive_dry='rsync --archive --itemize-changes --dry-run --exclude=.stversions --delete ~/mem-arch/ earthian@100.95.49.75:~/archive'
 
 ytdcur() { # yt-dlp download current
   url="$(wl-paste)"
