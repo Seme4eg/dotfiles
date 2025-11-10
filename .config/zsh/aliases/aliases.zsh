69df64c60b3751261ae002fc9ef852f7c01d2bdc
earthian <418@duck.com>
2025-11-11 00:43:26 +0200

    take some useful aliases from omarchy

--- .config/zsh/aliases/aliases.zsh
+++ .config/zsh/aliases/aliases.zsh
@@ -43,3 +43,31 @@ ytdcur() { # yt-dlp download current
   url="${url%?list*}"
   yt-dlp -x --output "${HOME}/Music/__dw/%(title)s.%(ext)s" "${url}"
 }
+
+# File system
+if command -v eza &>/dev/null; then
+  alias ls='eza -lh --group-directories-first --icons=auto'
+  alias lsa='ls -a'
+  alias lt='eza --tree --level=2 --long --icons --git'
+  alias lta='lt -a'
+fi
+
+alias ff="fzf --preview 'bat --style=numbers --color=always {}'"
+
+if command -v zoxide &>/dev/null; then
+  alias cd="zd"
+  zd() {
+    if [ $# -eq 0 ]; then
+      builtin cd ~ && return
+    elif [ -d "$1" ]; then
+      builtin cd "$1"
+    else
+      z "$@" && printf "\U000F17A9 " && pwd || echo "Error: Directory not found"
+    fi
+  }
+fi
+
+# Directories
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
