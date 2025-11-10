ac798adef6c08066ca5ccc5c82ec8c9640d3b9a4
earthian <418@duck.com>
2025-11-11 00:52:13 +0200

    ufw remove docker logic for now
    as i am using podman and don't know if ufw will be affecting it

--- install/first-run/firewall.sh
+++ install/first-run/firewall.sh
@@ -8,15 +8,10 @@ sudo ufw default allow outgoing
 # sudo ufw allow 53317/udp
 # sudo ufw allow 53317/tcp
 
-# Allow Docker containers to use DNS on host
-sudo ufw allow in proto udp from 172.16.0.0/12 to 172.17.0.1 port 53 comment 'allow-docker-dns'
-
 # Turn on the firewall
 sudo ufw --force enable
 
 # Enable UFW systemd service to start on boot
 sudo systemctl enable ufw
 
-# Turn on Docker protections
-sudo ufw-docker install
 sudo ufw reload
