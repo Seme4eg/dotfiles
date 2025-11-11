c51fbe01a96406b13027e181e2fc8a9e7f5679d7
earthian <418@duck.com>
2025-11-11 15:15:21 +0200

    first run mode don't need stub dns - breaks ovpn

--- install/pre-install/first-run-mode.sh
+++ install/pre-install/first-run-mode.sh
@@ -5,12 +5,9 @@ touch ~/.local/state/first-run.mode
 # Setup sudo-less access for first-run
 sudo tee /etc/sudoers.d/first-run >/dev/null <<EOF
 Cmnd_Alias FIRST_RUN_CLEANUP = /bin/rm -f /etc/sudoers.d/first-run
-Cmnd_Alias SYMLINK_RESOLVED = /usr/bin/ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 $USER ALL=(ALL) NOPASSWD: /usr/bin/systemctl
 $USER ALL=(ALL) NOPASSWD: /usr/bin/ufw
 $USER ALL=(ALL) NOPASSWD: /usr/bin/ufw-docker
-$USER ALL=(ALL) NOPASSWD: /usr/bin/gtk-update-icon-cache
-$USER ALL=(ALL) NOPASSWD: SYMLINK_RESOLVED
 $USER ALL=(ALL) NOPASSWD: FIRST_RUN_CLEANUP
 EOF
 sudo chmod 440 /etc/sudoers.d/first-run
