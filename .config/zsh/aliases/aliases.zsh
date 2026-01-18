b8dbae9f7bf6b5c9d50d9649461e793468307e5e
earthian <418@duck.com>
2026-01-19 01:27:33 +0400

    rsync function instead of redundant aliases

--- .config/zsh/aliases/aliases.zsh
+++ .config/zsh/aliases/aliases.zsh
@@ -30,9 +30,17 @@ alias homediff="diff <(tree -L 1 -a --dirsfirst) dotfiles/.local/share/HOME"
 # - if just want to see more info add --verbove (increases verbocity by one) and
 #   it is essentially the '--info=' flag with sane params for all the info you
 #   need. Run `rsync --info=help' to see all flags and levels`
-alias rsync_archive='rsync --archive --info=progress2 --exclude=.stversions --delete ~/mem-arch/ earthian@100.95.49.75:~/archive'
-alias rsync_archive_local='rsync --archive --info=progress2 --exclude=.stversions --delete ~/mem-arch/ earthian@192.168.1.81:~/archive'
+alias rsync_archive='my-rsync "earthian@100.95.49.75"'
+alias rsync_archive_local='my-rsync "earthian@192.168.1.81"'
 alias rsync_archive_dry='rsync --archive --itemize-changes --dry-run --exclude=.stversions --delete ~/mem-arch/ earthian@100.95.49.75:~/archive'
+
+my-rsync() {
+  # fuzzy params explanation:
+  # https://serverfault.com/questions/489289/handling-renamed-files-or-directories-in-rsync
+  rsync --archive --info=progress2 --exclude=.stversions \
+    --fuzzy --delay-updates --delete-delay \
+    --delete ~/mem-arch/ "$1:~/archive"
+}
 
 ytdcur() { # yt-dlp download current
   url="$(wl-paste)"
