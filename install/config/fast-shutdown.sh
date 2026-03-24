a559c2919567596b54c3d95e5c21bff158bd1b31
earthian <418@duck.com>
2026-03-24 23:17:51 +0500

    add fast shutdown service (c) omachi

--- install/config/fast-shutdown.sh
+++ install/config/fast-shutdown.sh
@@ -4,4 +4,12 @@ cat <<EOF | sudo tee /etc/systemd/system.conf.d/10-faster-shutdown.conf
 [Manager]
 DefaultTimeoutStopSec=5s
 EOF
+
+sudo mkdir -p /etc/systemd/system/user@.service.d
+
+cat <<EOF | sudo tee /etc/systemd/system/user@.service.d/faster-shutdown.conf
+[Manager]
+TimeoutStopSec=5s
+EOF
+
 sudo systemctl daemon-reload
