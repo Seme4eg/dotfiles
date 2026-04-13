1ef415977d0792e718d6c50bcb374865dc80e089
earthian <418@duck.com>
2026-04-13 23:36:41 +0500

    podman aliases update from homelab

--- .config/zsh/aliases/podman.zsh
+++ .config/zsh/aliases/podman.zsh
@@ -1,25 +1,26 @@
 #!/usr/bin/env zsh
 
-alias pd="podman"
-alias pds="pd start"
-alias pdstop="pd stop"
-alias pdl="pd logs --follow --since 10m"
-alias pdrm="pd rm -fv"
-alias pde="pd exec"         # <container_name> command (no quotes)
-alias pdexec="pd exec -it " # add -U <username>
-pdei() { p exec -it "$1" /bin/sh; }
+alias p="podman"
+alias pd="p stop"
+alias pstop="p stop"
+alias pe="p exec" # <container_name> command (no quotes)
+alias pl="p logs --follow --since 10m"
+# alias prm="pd rm -fv"
+
+pei() { p exec -it "$1" /bin/sh; }
 penv() { pde "$1" env; }
 
 # --- podman-compose ---
 
 alias pc="podman-compose"
 alias pcu="pc up -d"
-# # recreate the container with all its volumes
-# alias pcur="pcu --force-recreate"
-# alias pcr="pc restart"
-# alias pcd="pc down"
-# # stop container and all its volumes
-# alias pcdv="pc down -v"
+alias pcvu="pc --verbose up -d"
+# recreate the container with all its volumes
+alias pcur="pcu --force-recreate"
+alias pcr="pc restart"
+alias pcd="pc down"
+# stop container and all its volumes
+alias pcdv="pc down -v"
 
 # --- containers ---
 alias pdpostgres="pd run -d --replace --name postgres -p 5432:5432 \
