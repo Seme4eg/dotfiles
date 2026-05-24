16799ccffb033fbf64d6d5f5e8465cadd9d007db
earthian <418@duck.com>
2026-05-25 00:03:39 +0530

    add lenovo battery conservation service
    hack after my bat0 became bat1 and tlp stopped affecting charging
    threshold

--- install/config/hardware/lenovo.sh
+++ install/config/hardware/lenovo.sh
@@ -1,5 +1,28 @@
 # lenovo setup
 
-sudo pacman -S --noconfirm vulkan-intel lib32-vulkan-intel bolt
-sudo ln --force $HOME/.config/tlp/02-lenovo.conf /etc/tlp.d/
-sudo systemctl --now enable tlp.service
+# sudo pacman -S --noconfirm vulkan-intel lib32-vulkan-intel bolt
+# sudo ln --force $HOME/.config/tlp/02-lenovo.conf /etc/tlp.d/
+# sudo systemctl --now enable tlp.service
+
+# FIXME: overheating event likely caused the Embedded Controller (EC) to
+# re-index the hardware registry so now tlp doesn't do the battery charging
+# threshold. 'sudo tlp-stat -b' prints BAT1, but setting threshold for BAT1 in
+# tlp config and 'sudo tlp start' errors out.
+# So now need to do it manually:
+sudo cat >"/etc/systemd/system/bat-conserv.service" <<EOF
+[Unit]
+Description=Enable Lenovo Battery Conservation Mode
+After=multi-user.target
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/bash -c 'echo 1 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode'
+ExecStop=/usr/bin/bash -c 'echo 0 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode'
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable --now bat-conserv.service
