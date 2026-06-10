ea912dee8b45ea427fedb9585e42b1fc5a10d4e8
earthian <418@duck.com>
2026-06-11 00:25:23 +0300

    amd udev installation part for power management

--- install/config/hardware/amd.sh
+++ install/config/hardware/amd.sh
@@ -11,3 +11,25 @@ sudo pacman -S --noconfirm --needed --asdeps mesa mesa-utils lib32-mesa mesa-vdp
   libva-mesa-driver vulkan-radeon lib32-vulkan-radeon
 yay -S --noconfirm amdgpu_top-bin
 vulkaninfo --summary | grep -q OPEN_SOURCE && sudo pacman -Rns amdvlk lib32-amdvlk
+
+# --- Udev rule to prevent my eGPU being slow on re-plug: ---
+
+AMD_GPU_ID=$(lspci -d ::03xx | grep 'AMD' | cut -f1 -d' ')
+
+if [ -z $AMD_GPU_ID ]; then
+  echo NO AMD GPU connected
+  exit 1
+fi
+
+# Disable AMD GPU power management to prevent lock-ups after wake
+RULE_PATH="/etc/udev/rules.d/99-amd-gpu-power.rules"
+UDEV_RULE="$(
+  cat <<EOF
+SUBSYSTEM=="pci", ATTR{vendor}=="0x1002", ATTR{device}=="$AMD_GPU_ID", ATTR{power/control}="on"
+EOF
+)"
+
+echo "$UDEV_RULE" | sudo tee "$RULE_PATH"
+
+sudo udevadm control --reload
+sudo udevadm trigger
