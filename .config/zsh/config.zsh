ec7e9f2a92e937fcf6e7968607eec392a936230f
earthian <418@duck.com>
2026-06-24 01:43:24 +0300

    remove unaffective java_options env var

--- .config/zsh/config.zsh
+++ .config/zsh/config.zsh
@@ -66,7 +66,6 @@ export PNPM_HOME=$XDG_DATA_HOME/pnpm
 export W3M_DIR="$XDG_STATE_HOME/w3m"
 export PARALLEL_HOME="$XDG_CONFIG_HOME"/parallel
 export PSQL_HISTORY="$XDG_STATE_HOME/psql_history"
-export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
 export WGETRC="$XDG_CONFIG_HOME/wgetrc"
 export CLAUDE_CONFIG_DIR="$XDG_CONFIG_HOME"/claude
 
